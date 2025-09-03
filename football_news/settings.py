7a4a58d4276e4116f1c696ffab5dbc650968af89
nitapb <nitapasaribu01@gmail.com>
2025-09-03 19:26:07 +0700

    menambahkan template main

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-(_pi9$%qw0z1=m2#j$eug!)#%3=(7+=dpn5f%c@)-273q=9l71
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", https://pbp.cs.ui.ac.id/nita.pasaribu/footballnews]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nita-pasaribu-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
